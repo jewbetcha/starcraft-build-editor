9b0c02c5dc9498ffebe6de6d54677bb61d9409c6
Coleman Rollins <coleman@ten7.com>
2015-03-06 16:13:48 -0600

    Added some functionality for total supply and collection rate, some styles, supply cap doesn't work yet...

--- src/js/js.js
+++ src/js/js.js
@@ -3,6 +3,10 @@
     $('.dropdown-button').dropdown();
     $('select').material_select();
 
+    //Globally define some vars
+    var total_supply = 0;
+    var supply_cap = 10;
+    var collect_rate = 0;
     // Add army units into JSON function as array of arrays
     var units_army = '{ "Marine": { "cost": 50, "costGas": 0, "time": 25, "supply": 1}, "Reaper": { "cost": 50, "costGas": 50, "time": 40, "supply": 1}, "Marauder": { "cost": 100, "costGas": 25, "time": 30, "supply": 2}, "Worker": { "cost": 50, "costGas": 0, "time": 17, "supply": 1} }';
     var army = JSON.parse(units_army);
@@ -20,7 +24,7 @@
         $('#supply').append(army[unit].supply + '</br>');
 
         // Count up stats
-        var total_supply = 0;
+
         $.each(selected,function() {
             total_supply += this.supply;
         });
@@ -30,7 +34,7 @@
         });
 
 
-        $('#total-supply').text(total_supply + "/" );
+        $('#total-supply').text(total_supply + "/" + supply_cap);
 
         // If game time is +60 seconds, make it in minutes
         if (total_time > 60) {
@@ -42,8 +46,8 @@
         	$('#elapsed').text(total_time + " in game seconds");
         }
         // If user adds a worker, add resource collection time
-        var collect_rate = 0;
-        if (unit = "Worker") {
+
+        if ( unit == "Worker") {
 
             collect_rate += 35;
 
@@ -53,7 +57,7 @@
     });
 
     // Now for structures
-    var units_structures = '{ "Supply Depot": { "cost": 100, "costGas": 0, "time": 30, "supply": 0} }';
+    var units_structures = '{ "Supply Depot": { "cost": 100, "costGas": 0, "time": 30, "supply": 0}, "Barracks": { "cost": 150, "costGas": 0, "time": 65, "supply": 0} }';
     var structures = JSON.parse(units_structures);
     var selected_structure = [];
 
@@ -71,8 +75,12 @@
         $('#supply').append(structures[building].supply + '</br>');
 
 
+        // Keep track of supply cap, may or may not work...
+        if ( building == "Supply Depot" ) {
+            supply_cap += 10;
 
-
+            console.log(supply_cap);
+        }
     });
 
 
